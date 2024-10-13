604a2004d4b460415bd4720febb17d2ba8c19679
mechariiii <mechari01@gmail.com>
2024-10-13 09:20:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-document.getElementById('dartboard').addEventListener('click', function() {
-    alert('This is a placeholder for the dartboard interaction.');
-});
+window.onload = function() {
+    var canvas = document.getElementById('dartboardCanvas');
+    var ctx = canvas.getContext('2d');
+
+    var boardColors = ['green', 'red', 'black', 'white'];
+    var centerColors = ['red', 'green'];
+    var radius = canvas.width / 2;
+    var numberOfSections = 20;
+
+    // رسم القطاعات
+    for (let i = 0; i < numberOfSections; i++) {
+        let angle = (i * Math.PI * 2) / numberOfSections;
+        let nextAngle = ((i + 1) * Math.PI * 2) / numberOfSections;
+
+        ctx.beginPath();
+        ctx.moveTo(radius, radius);
+        ctx.arc(radius, radius, radius, angle, nextAngle);
+        ctx.fillStyle = boardColors[i % boardColors.length];
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    // رسم المركز
+    ctx.beginPath();
+    ctx.arc(radius, radius, radius / 10, 0, Math.PI * 2);
+    ctx.fillStyle = centerColors[0];
+    ctx.fill();
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.arc(radius, radius, radius / 20, 0, Math.PI * 2);
+    ctx.fillStyle = centerColors[1];
+    ctx.fill();
+    ctx.closePath();
+};
