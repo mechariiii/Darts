ef1f63f01681416547c770c5f65d9f29b984c116
mechariiii <mechari01@gmail.com>
2024-10-13 09:23:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 window.onload = function() {
     var canvas = document.getElementById('dartboardCanvas');
     var ctx = canvas.getContext('2d');
-
-    var boardColors = ['green', 'red', 'black', 'white'];
-    var centerColors = ['red', 'green'];
     var radius = canvas.width / 2;
-    var numberOfSections = 20;
 
-    // رسم القطاعات
-    for (let i = 0; i < numberOfSections; i++) {
-        let angle = (i * Math.PI * 2) / numberOfSections;
-        let nextAngle = ((i + 1) * Math.PI * 2) / numberOfSections;
-
+    function drawSection(startAngle, endAngle, color) {
         ctx.beginPath();
         ctx.moveTo(radius, radius);
-        ctx.arc(radius, radius, radius, angle, nextAngle);
-        ctx.fillStyle = boardColors[i % boardColors.length];
-        ctx.fill();
+        ctx.arc(radius, radius, radius, startAngle, endAngle);
         ctx.closePath();
+        ctx.fillStyle = color;
+        ctx.fill();
+    }
+
+    // الألوان الدائرية
+    var colors = ['green', 'red', 'black', 'white'];
+    var sectionAngle = (2 * Math.PI) / 20;
+
+    // رسم القطاعات الـ 20
+    for (var i = 0; i < 20; i++) {
+        var startAngle = i * sectionAngle;
+        var endAngle = startAngle + sectionAngle;
+        var color = colors[i % colors.length];
+        drawSection(startAngle, endAngle, color);
     }
 
     // رسم المركز
     ctx.beginPath();
-    ctx.arc(radius, radius, radius / 10, 0, Math.PI * 2);
-    ctx.fillStyle = centerColors[0];
+    ctx.arc(radius, radius, radius / 10, 0, 2 * Math.PI);
+    ctx.fillStyle = 'red';
     ctx.fill();
     ctx.closePath();
 
     ctx.beginPath();
-    ctx.arc(radius, radius, radius / 20, 0, Math.PI * 2);
-    ctx.fillStyle = centerColors[1];
+    ctx.arc(radius, radius, radius / 20, 0, 2 * Math.PI);
+    ctx.fillStyle = 'green';
     ctx.fill();
     ctx.closePath();
 };
